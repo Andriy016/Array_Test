ab9215839c03ac406cdbc6e79b6930ced588f27f
admin <admin@Andrii-MacBook-Air.local>
2018-01-30 13:57:26 +0300

    init repo

---
+++ src/Randomm.java
@@ -0,0 +1,14 @@
+public final class Randomm {
+
+    private static final int ARRAY_SIZE = 100;
+
+    public static void main(String[] args) {
+        int[] myIntArray = new int[ARRAY_SIZE];
+        for (int i=0; i < ARRAY_SIZE; i++);
+
+
+        System.out.println (ARRAY_SIZE);
+
+
+    }
+}
